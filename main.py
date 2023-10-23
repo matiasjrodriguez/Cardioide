aa096a2b3aea1312ece91d3d215846b658f4c648
matiasjrodriguez <mellarodriguez29@gmail.com>
2023-10-23 18:29:00 -0300

    agregar método draw

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ class App:
     def __init__(self):
         self.screen = pygame.display.set_mode([1600, 900])
         self.clock = pygame.time.Clock()
+        
+    def draw(self):
+        self.screen.fill('black')
+        pygame.display.flip()
+        
